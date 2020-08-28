d3a8a20664688fb995e889e8f87619dd31097b26
Martin Briceno <martin.briceno@live.com>
2020-08-28 14:10:30 -0400

    [home] Add routes to component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import './App.css';
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
+
 import Home from './Home'
+import Contact from './Contact'
+import Procedures from './Procedures'
+
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      <Home />
+      <Route exact path="/" component={Home} />
+      <Route path="/procedures" component={Procedures} />
+      <Route path="/contact" component={Contact} />
+      {/* <Home />
+      <Procedures />
+      <Contact /> */}
     </div>
+    </Router>
   );
 }
 
---
+++ src/Contact.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Contact extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Contacts</h1>
+                <p>Martin Briceno MD</p>
+                <p>111-111-1111 </p>
+            </div>
+        )
+    }
+}
+
+export default Contact;
