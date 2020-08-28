c6e6005ff37ac702c6688945611bd2f26a4803f9
Martin Briceno <martin.briceno@live.com>
2020-08-28 14:56:57 -0400

    [WidgetShow] Add WidgetShow component and render

--- src/App.js
+++ src/App.js
@@ -10,15 +10,30 @@ import {
 import Home from './Home'
 import Contact from './Contact'
 import Procedures from './Procedures'
-
+import WidgetShow from './WidgetShow'
 
 function App() {
+  const procedures = [
+    "Deep Cleaning",
+    "Oral Surgery",
+    "Root Canal",
+    "Crown",
+    "Implants",
+    "Fillings"
+  ]
+
   return (
     <Router>
     <div className="App">
+      <nav>
+        <Link to="/"><button>Home</button></Link>{'      '}
+        <Link to="/procedures"><button>Procedures</button></Link>{'      '}
+        <Link to="/contact"><button>Contact Us</button></Link>
+      </nav>
       <Route exact path="/" component={Home} />
-      <Route path="/procedures" component={Procedures} />
+      <Route path="/procedures" render={()=><Procedures procedures={procedures}/>}/>
       <Route path="/contact" component={Contact} />
+      <Route path="/widgets/:id" render={(props) => <WidgetShow procedures={procedures} {...props}/>}/>
       {/* <Home />
       <Procedures />
       <Contact /> */}
