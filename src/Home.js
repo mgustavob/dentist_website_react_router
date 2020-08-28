bccf33a68a5b8b4961770776d018c0b63c3c5db8
Martin Briceno <martin.briceno@live.com>
2020-08-28 13:52:48 -0400

    [home] Add component

---
+++ src/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Martin Dentistry</h1>
+                <p>Were the dentist go for their dental needs.</p>
+            </div>
+        )
+    }
+}
+
+export default Home;
